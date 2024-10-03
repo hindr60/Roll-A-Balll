680bcbba5d5422f786d5a0ddf406da882692e2c4
hindr60 <azrat1544@gmail.com>
2024-10-03 11:37:27 +1000

    Added pickups
    Also have developed a bobbing animation for the objects.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,18 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     public float speed = 1f;
+    private int pickupCount;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Gets the rigidbody component attached to this game object
         rb = GetComponent<Rigidbody>();
+        //Get ther number of pickups in our scene
+        pickupCount = GameObject.FindGameObjectsWithTag("Pickup").Length;
+        //Run the CheckPickups function
+        CheckPickups();
+
     }
     void FixedUpdate()
     {
@@ -19,10 +26,30 @@ public class PlayerController : MonoBehaviour
 
         Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
         rb.AddForce(movement * speed);
-    }
-    // Update is called once per frame
-    void Update()
-    {
 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Pickup") 
+        {
+            //Destroy the collided object
+            Destroy(other.gameObject);
+            pickupCount--;
+            //Run the CheckPickups function again
+            CheckPickups();
+        }
+    }
+    private void CheckPickups()
+    {
+        print("Pickups left: " + pickupCount);
+        if(pickupCount == 0)
+        {
+            WinGame();
+        }
+    }
+    private void WinGame()
+    {
+        print("Yipee! You win.");
     }
 }
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+using static UnityEditor.PlayerSettings;
+
+public class Rotator : MonoBehaviour
+{
+    float originalY;
+    public float speed = 1f;
+    public float floatStrength = 1f;
+    public Vector3 rotationValue = new Vector3(15, 30, 45);
+    void Start()
+    {
+        this.originalY = this.transform.position.y;
+    }
+
+    void Update()
+    {
+        //Rotate the object
+        transform.Rotate(rotationValue * Time.deltaTime * speed); //The new vector determines the direction it is spinning. 
+
+        transform.position = new Vector3(transform.position.x,originalY + ((float)Math.Sin(Time.time) * floatStrength ),transform.position.z);
+ 
+    }
+}
+ 
